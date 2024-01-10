c399ea1884941db23b6c7764ebd7b1bf47d82781
Murph Murphy <murph@clurictec.com>
2024-01-10 16:42:31 -0700

    Try running with ironcore, add async

--- main.py
+++ main.py
@@ -12,40 +12,43 @@ from random import random
 from skimage import io
 import sys
 from tqdm import tqdm
+import ironcore_alloy
+import asyncio
 
 sys.path.append("./stylegan2-ada-pytorch")
 
+
 # LFW functions taken from David Sandberg's FaceNet implementation
 def distance(embeddings1, embeddings2, distance_metric=0):
-    if distance_metric==0:
+    if distance_metric == 0:
         # Euclidian distance
         diff = np.subtract(embeddings1, embeddings2)
-        dist = np.sum(np.square(diff),1)
-    elif distance_metric==1:
+        dist = np.sum(np.square(diff), 1)
+    elif distance_metric == 1:
         # Distance based on cosine similarity
         dot = np.sum(np.multiply(embeddings1, embeddings2), axis=1)
         norm = np.linalg.norm(embeddings1, axis=1) * np.linalg.norm(embeddings2, axis=1)
         similarity = dot / norm
         dist = np.arccos(similarity) / math.pi
     else:
-        raise 'Undefined distance metric %d' % distance_metric
+        raise "Undefined distance metric %d" % distance_metric
 
     return dist
 
+
 class FaceReconstruction:
-    
-    def __init__(self, device='cuda'):
+    def __init__(self, device="cuda"):
         self.device = device
-        
+
         self.pregen_latents = None
         self.pregen_embeddings = None
-        
+
         #
         # Load Networks
         #
-        
+
         # Face recognition
-        self.resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+        self.resnet = InceptionResnetV1(pretrained="vggface2").eval().to(device)
 
         # Face detect/aling/crop
         self.mtcnn = MTCNN(
@@ -56,17 +59,17 @@ class FaceReconstruction:
         )
 
         # StyleGAN
-        with open('ffhq.pkl', 'rb') as f:
-            G = pickle.load(f)['G_ema'].to(device)  # torch.nn.Module
+        with open("ffhq.pkl", "rb") as f:
+            G = pickle.load(f)["G_ema"].to(device)  # torch.nn.Module
         self.G = G.eval()
-        
+
     def gen_stylegan_latents(self, batch_size=8):
-        z = torch.randn([batch_size, self.G.z_dim]).to(self.device)    # latent codes
+        z = torch.randn([batch_size, self.G.z_dim]).to(self.device)  # latent codes
         return z
 
     def infer_stylegan_faces(self, z):
         """
-            z: latent vector(s) of dimension (batch_size, G.z_dim)
+        z: latent vector(s) of dimension (batch_size, G.z_dim)
         """
         z = z.to(self.device)
 
@@ -74,11 +77,10 @@ class FaceReconstruction:
             z = z.unsqueeze(0)
 
         with torch.no_grad():
-            c = None                                # class labels (not used in this example)
+            c = None  # class labels (not used in this example)
             w = self.G.mapping(z, c, truncation_psi=0.5, truncation_cutoff=8)
-            img = self.G.synthesis(w, noise_mode='const', force_fp32=True)
+            img = self.G.synthesis(w, noise_mode="const", force_fp32=True)
         return img
-
 
     def postprocess_stylegan_faces(self, img):
         """
@@ -89,11 +91,10 @@ class FaceReconstruction:
         img_proc = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         return img_proc
 
-
     def get_facenet_embedding(self, img):
         """
-            img: pytorch tensor of images of shape (batch_size, 1024, 1024, 3). 
-                 Should be a tensor of type uint8, with integer values ranging from 0 to 255
+        img: pytorch tensor of images of shape (batch_size, 1024, 1024, 3).
+             Should be a tensor of type uint8, with integer values ranging from 0 to 255
         """
 
         assert img.shape[-1] == 3 and (len(img.shape) == 4 or len(img.shape) == 3)
@@ -106,13 +107,14 @@ class FaceReconstruction:
 
         with torch.no_grad():
             im_crop = self.mtcnn(img.cpu())
-            im_all = torch.cat([im.unsqueeze(0) for im in im_crop if im is not None], dim=0)
+            im_all = torch.cat(
+                [im.unsqueeze(0) for im in im_crop if im is not None], dim=0
+            )
             img_embedding = self.resnet(im_all.to(self.device))
 
         return img_embedding
 
-
-    def generate_and_save(self, n=8000, batch_size=16, file_name='pregenerated.pt'):
+    def generate_and_save(self, n=8000, batch_size=16, file_name="pregenerated.pt"):
         iters = n // batch_size
 
         with torch.no_grad():
@@ -133,16 +135,18 @@ class FaceReconstruction:
         torch.save([all_latents, all_embeddings], file_name)
 
     def run_pregeneration_routine(self):
-        """ 
+        """
         Pregenerates 160k (latent vector, embedding) pairs for faster initial matching.
         """
         for i in range(20):
-            self.generate_and_save(n=8000, batch_size=16, file_name='pregenerated_'+str(i)+'.pt')
+            self.generate_and_save(
+                n=8000, batch_size=16, file_name="pregenerated_" + str(i) + ".pt"
+            )
 
     def load_pregenerated(self):
         """
         Load pregenerated vectors
-        
+
         Returns:
             all_latents: (n, 512) list of latent vectors
             all_embeddings: (n, 512) list of FaceNet embedding vectors
@@ -150,7 +154,9 @@ class FaceReconstruction:
         all_latents = []
         all_embeddings = []
         for i in range(20):
-            latents, embeddings = torch.load('pregenerated/pregenerated_'+str(i)+'.pt')
+            latents, embeddings = torch.load(
+                "pregenerated/pregenerated_" + str(i) + ".pt"
+            )
             all_latents.append(latents)
             all_embeddings.append(embeddings)
 
@@ -161,34 +167,44 @@ class FaceReconstruction:
     def find_closest_pregen(self, emb, pregen_latents, pregen_embeddings, offset=0):
         """
         Finds closest pregenerated match for given facenet embedding. Looks through
-        a list of pregenerated (latent vector, embedding) pairs, and returns the 
+        a list of pregenerated (latent vector, embedding) pairs, and returns the
         latent vector whose facenet embedding is the closest to the target.
-        
+
         Args:
             emb: Target embedding to find closest latent vector to.
             pregen_latents: List of latent vectors
             pregen_embeddings: Associated list of embedding vectors
-            
+
         Returns:
             (latent, embedding): tuple of latent vector and facenet embedding
         """
-        norms = (pregen_embeddings-emb.to(pregen_embeddings)).norm(dim=1)
+        norms = (pregen_embeddings - emb.to(pregen_embeddings)).norm(dim=1)
         sorted_idxs = norms.argsort()
         best_idx = sorted_idxs[offset]
-        print('best norm: ', norms[best_idx])
+        print("best norm: ", norms[best_idx])
         return pregen_latents[best_idx], pregen_embeddings[best_idx]
 
-
-    def perform_face_reconstruction(self, target_emb, pregen=True, pregen_offset=0, init_zeros=False, 
-                                    iters=400, use_annealing=False, std_multiplier=0.98):
+    async def perform_face_reconstruction(
+        self,
+        target_emb,
+        pregen=True,
+        encrypt=False,
+        pregen_offset=0,
+        init_zeros=False,
+        iters=400,
+        use_annealing=False,
+        std_multiplier=0.98,
+    ):
         """Performs the face reconstruction algorithm
 
         Arguments:
             target_emb: torch.Tensor
-                A Tensor of shape (1, 512), the target tensor to reconstruct a face for. 
+                A Tensor of shape (1, 512), the target tensor to reconstruct a face for.
             pregen: Boolean
                 Whether to start from the closest pregenerated point.
                 If false, will start from a random point.
+            encrypt: Boolean
+                Whether to encrypt the embedding or not before attempting the reconstruction.
             pregen_offset: int
                 If pregen, will use n-th-to-best pregenerated match (default 0)
             init_zeros: Boolean
@@ -211,18 +227,46 @@ class FaceReconstruction:
             best_latent_list: list of latent vectors at each improvement
             best_emb_list: list of embedding vectors at each improvement
         """
-        
+
         def safe_exp(x):
-            try: return exp(x)
-            except: return 0
+            try:
+                return exp(x)
+            except:
+                return 0
 
         def P(e, e_prime, T):
-
             if e_prime < e:
                 return 1
 
             else:
-                return safe_exp(-(e_prime-e)/T)
+                return safe_exp(-(e_prime - e) / T)
+
+        # If encrypt is on, encrypt the embedding and bring it back to a Tensor before running
+        if encrypt:
+            # initialize IronCore Alloy
+            key_bytes = b"somereallylongandverysecurekeyinsteadofanexamplestring"
+            approximation_factor = 5
+            vector_secrets = {
+                "facial_recognition": VectorSecret(
+                    approximation_factor,
+                    RotatableSecret(StandaloneSecret(1, Secret(key_bytes)), None),
+                ),
+            }
+            standard_secrets = StandardSecrets(None, [])
+            deterministic_secrets = {}
+            config = StandaloneConfiguration(
+                standard_secrets, deterministic_secrets, vector_secrets
+            )
+            sdk = Standalone(config)
+            embedding_vector = target_emb.tolist()[0]
+            encrypted_vector = await sdk.vector().encrypt(
+                PlaintextVector(embedding_vector, "facial_recognition", ""),
+                AlloyMetadata.new_simple(""),
+            )
+            print(encrypted_vector)
+            encrypted_vector = encrypted_vector.encrypted_vector
+            target_emb = torch.FloatTensor(encrypted_vector)
+            print(target_emb)
 
         # Make sure we're on gpu
         target_emb = target_emb.to(self.device)
@@ -231,18 +275,23 @@ class FaceReconstruction:
         if init_zeros:
             best_latent = torch.zeros([1, self.G.z_dim]).cuda()
         else:
-            best_latent = torch.randn([1, self.G.z_dim]).cuda() 
+            best_latent = torch.randn([1, self.G.z_dim]).cuda()
         best_norm = 1e7
 
         if pregen:
             if self.pregen_latents is None or self.pregen_embeddings is None:
                 print("ERROR: Pregenerated latents and embeddings are not loaded")
                 return
-            
-            best_latent, best_embedding = self.find_closest_pregen(target_emb, self.pregen_latents, self.pregen_embeddings, offset=pregen_offset)
+
+            best_latent, best_embedding = self.find_closest_pregen(
+                target_emb,
+                self.pregen_latents,
+                self.pregen_embeddings,
+                offset=pregen_offset,
+            )
             best_latent = best_latent.to(self.device)
             best_embedding = best_embedding.to(self.device)
-            best_norm = (target_emb-best_embedding).norm()
+            best_norm = (target_emb - best_embedding).norm()
 
         current_latent = best_latent
         current_norm = best_norm
@@ -255,23 +304,26 @@ class FaceReconstruction:
 
         # Add the image of the starting latent to the list
         with torch.no_grad():
-            best_face = self.postprocess_stylegan_faces(self.infer_stylegan_faces(current_latent))[0].cpu()
+            best_face = self.postprocess_stylegan_faces(
+                self.infer_stylegan_faces(current_latent)
+            )[0].cpu()
         best_list.append(best_face)
         best_norm_list.append(best_norm)
         best_latent_list.append(best_latent)
         best_emb_list.append(self.get_facenet_embedding(best_face))
 
-        print('Starting norm: ', best_norm)
+        print("Starting norm: ", best_norm)
         std = 1
         T = 0
 
         with torch.no_grad():
             for i in range(iters):
-
                 if use_annealing:
-                    T = 1 - (i+1)/iters
+                    T = 1 - (i + 1) / iters
 
-                neighbor_latents = current_latent + std*torch.randn([16, self.G.z_dim]).cuda()
+                neighbor_latents = (
+                    current_latent + std * torch.randn([16, self.G.z_dim]).cuda()
+                )
                 faces = self.infer_stylegan_faces(neighbor_latents)
                 faces_pp = self.postprocess_stylegan_faces(faces)
                 embeddings = self.get_facenet_embedding(faces_pp)
@@ -281,12 +333,12 @@ class FaceReconstruction:
 
                 if P(current_norm, norms[best_idx], T) > random():
                     if use_annealing:
-                            print('[annealing] new current:', norms[best_idx].item())
+                        print("[annealing] new current:", norms[best_idx].item())
                     current_latent = neighbor_latents[best_idx]
                     current_norm = norms[best_idx]
 
                 if norms[best_idx] < best_norm:
-                    print('New best:', norms[best_idx].item())
+                    print("New best:", norms[best_idx].item())
                     best_latent = neighbor_latents[best_idx]
                     best_norm = norms[best_idx]
 
@@ -300,15 +352,35 @@ class FaceReconstruction:
         return best_latent, best_list, best_norm_list, best_latent_list, best_emb_list
 
 
-if __name__ == "__main__":
+async def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--setup", action="store_true", help="Pregenerate 160k embeddings (stored in the same folder)")
-    parser.add_argument("--pregen", action="store_true", help="Use pregenerated embedding to select a starting location")
-    parser.add_argument("--anneal", action="store_true", help="Use simulated annealing in the reconstruction algorithm (by default, will use greedy algorithm.")
+    parser.add_argument(
+        "--setup",
+        action="store_true",
+        help="Pregenerate 160k embeddings (stored in the same folder)",
+    )
+    parser.add_argument(
+        "--pregen",
+        action="store_true",
+        help="Use pregenerated embedding to select a starting location",
+    )
+    parser.add_argument(
+        "--anneal",
+        action="store_true",
+        help="Use simulated annealing in the reconstruction algorithm (by default, will use greedy algorithm.",
+    )
     parser.add_argument("--iters", type=int, default=400, help="Number of iterations")
     parser.add_argument("--img", type=str, help="Image path for reconstruction")
-    parser.add_argument("--save_details", action="store_true", help="Save .pt file with detailed results")
-    parser.add_argument("--encrypt", action="store_true", help="Encrypt the embedding before reconstructing. Saves to output_encrypted.png")
+    parser.add_argument(
+        "--save_details",
+        action="store_true",
+        help="Save .pt file with detailed results",
+    )
+    parser.add_argument(
+        "--encrypt",
+        action="store_true",
+        help="Encrypt the embedding before reconstructing. Saves to output_encrypted.png",
+    )
 
     args = parser.parse_args()
 
@@ -317,42 +389,61 @@ if __name__ == "__main__":
     if args.anneal:
         print("Running with annealing")
     print(args)
-        
+
     face_reconstruction = FaceReconstruction()
-    
+
     if args.setup:
         print("Running setup procedure...")
         face_reconstruction.run_pregeneration_routine()
-        
-    else: 
+
+    else:
         if args.pregen:
             face_reconstruction.load_pregenerated()
 
         image_paths = [args.img]
-        images_pil = [Image.open(image_path).convert('RGB') for image_path in image_paths]
-        images_pil_crop = [face_reconstruction.mtcnn(im).to('cuda') for im in images_pil]
-        images_pil_crop_pp = [im.detach().cpu().permute(1, 2, 0)*0.5+0.5 for im in images_pil_crop]
+        images_pil = [
+            Image.open(image_path).convert("RGB") for image_path in image_paths
+        ]
+        images_pil_crop = [
+            face_reconstruction.mtcnn(im).to("cuda") for im in images_pil
+        ]
+        images_pil_crop_pp = [
+            im.detach().cpu().permute(1, 2, 0) * 0.5 + 0.5 for im in images_pil_crop
+        ]
 
         with torch.no_grad():
-            target_embeddings = [face_reconstruction.resnet(im.unsqueeze(0)).cpu() for im in images_pil_crop]
+            target_embeddings = [
+                face_reconstruction.resnet(im.unsqueeze(0)).cpu()
+                for im in images_pil_crop
+            ]
 
         target_emb = target_embeddings[0]
         if args.encrypt:
             embedding_list = target_emb.tolist()[0]
             # encrypt embedding_list using ironcore_alloy, turn into a Tensor of shape (1, 512)
-            # then assign that to target_emb 
+            # then assign that to target_emb
             pass
-        result = face_reconstruction.perform_face_reconstruction(target_emb, pregen=args.pregen, init_zeros=False, 
-                                             use_annealing=args.anneal, iters=args.iters, std_multiplier=0.992)
-        
+        result = await face_reconstruction.perform_face_reconstruction(
+            target_emb,
+            pregen=args.pregen,
+            init_zeros=False,
+            use_annealing=args.anneal,
+            iters=args.iters,
+            std_multiplier=0.992,
+        )
+
         best_latent, best_list, best_norm_list, best_latent_list, best_emb_list = result
         im = Image.fromarray(best_list[-1].detach().cpu().numpy())
         if args.encrypt:
             im.save("output_encrypted.png")
         else:
             im.save("output.png")
-        
-        if args.save_details:
-            saved_output = {'targets': target_embeddings, 'results': results}
-            torch.save(saved_output, 'result.pt')
 
+        if args.save_details:
+            saved_output = {"targets": target_embeddings, "results": results}
+            torch.save(saved_output, "result.pt")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
